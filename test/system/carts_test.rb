042ccdb803a507d03a303a2da641a3cdda3ca053
Santiago Rodríguez <santo1996.29@gmail.com>
2020-05-04 06:59:01 -0400

    add system tests for orders and carts

--- test/system/carts_test.rb
+++ test/system/carts_test.rb
@@ -1,41 +1,21 @@
 require "application_system_test_case"
 
 class CartsTest < ApplicationSystemTestCase
-  setup do
-    @cart = carts(:one)
+  test 'add to cart reveals the cart' do
+    visit store_index_url
+    assert_no_selector '#cart'
+    click_on 'Add to cart', match: :first
+    assert_selector '#cart'
   end
 
-  test "visiting the index" do
-    visit carts_url
-    assert_selector "h1", text: "Carts"
-  end
+  test 'empty cart hides the cart' do
+    visit store_index_url
+    click_on 'Add to cart', match: :first
 
-  test "creating a Cart" do
-    visit carts_url
-    click_on "New Cart"
-
-    click_on "Create Cart"
-
-    assert_text "Cart was successfully created"
-    click_on "Back"
-  end
-
-  test "updating a Cart" do
-    visit carts_url
-    click_on "Edit", match: :first
-
-    click_on "Update Cart"
-
-    assert_text "Cart was successfully updated"
-    click_on "Back"
-  end
-
-  test "destroying a Cart" do
-    visit carts_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
+    accept_alert do
+      click_on 'Empty cart'
     end
 
-    assert_text "Cart was successfully destroyed"
+    assert_no_selector '#cart'
   end
 end
--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,49 +1,41 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
-  setup do
-    @order = orders(:one)
+  test 'check routing number' do
+    visit store_index_url
+    click_on 'Add to cart', match: :first
+    click_on 'Checkout'
+    fill_in 'order_name', with: 'Santiago Rodríguez'
+    fill_in 'order_address', with: '123 Calle las Palmas'
+    fill_in 'order_email', with: 'santiago@example.com'
+    assert_no_selector '#order_routing_number'
+    select 'Check', from: 'Pay type'
+    assert_selector '#order_routing_number'
   end
 
-  test "visiting the index" do
-    visit orders_url
-    assert_selector "h1", text: "Orders"
+  test 'credit card fields' do
+    visit store_index_url
+    click_on 'Add to cart', match: :first
+    click_on 'Checkout'
+    fill_in 'order_name', with: 'Santiago Rodríguez'
+    fill_in 'order_address', with: '123 Calle las Palmas'
+    fill_in 'order_email', with: 'santiago@example.com'
+    assert_no_selector '#order_credit_card_number'
+    assert_no_selector '#order_expiry_date'
+    select 'Credit card', from: 'Pay type'
+    assert_selector '#order_credit_card_number'
+    assert_selector '#order_expiration_date'
   end
 
-  test "creating a Order" do
-    visit orders_url
-    click_on "New Order"
-
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.pay_type
-    click_on "Create Order"
-
-    assert_text "Order was successfully created"
-    click_on "Back"
-  end
-
-  test "updating a Order" do
-    visit orders_url
-    click_on "Edit", match: :first
-
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.pay_type
-    click_on "Update Order"
-
-    assert_text "Order was successfully updated"
-    click_on "Back"
-  end
-
-  test "destroying a Order" do
-    visit orders_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Order was successfully destroyed"
+  test 'purchase order number' do
+    visit store_index_url
+    click_on 'Add to cart', match: :first
+    click_on 'Checkout'
+    fill_in 'order_name', with: 'Santiago Rodríguez'
+    fill_in 'order_address', with: '123 Calle las Palmas'
+    fill_in 'order_email', with: 'santiago@example.com'
+    assert_no_selector '#order_po_number'
+    select 'Purchase order', from: 'Pay type'
+    assert_selector '#order_po_number'
   end
 end
